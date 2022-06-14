d803e5c07677fd237c7ecde393692f3b063aeff9
Rushnan Alam <ralam@ralam-mbp.jnpr.net>
2022-06-13 17:14:45 -0700

    second commit

--- src/comps/Button/Button.tsx
+++ src/comps/Button/Button.tsx
@@ -3,50 +3,59 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 
-export default class Button extends Component{
+
+// add an interface containing all attribues/props used in the Button class
+interface ButtonAttributes{
+  type: any,
+  onClick(): any,
+  buttonStyle: string,
+  buttonSize: string, 
+  children: string
+}
+
+interface ButtonProps{
+  buttonType: ButtonAttributes[];
+}
+
+
+
+
+export default class Button extends Component<ButtonProps, any>{
     private styles = ['btn--primary', 'btn--outline'];
     private sizes = ['btn--medium', 'btn--large'];
-    private children?;
-    private type?;
-    private onClick?;
-    private buttonStyle;
-    private buttonSize;
-
-
-
+    
     constructor(props: any){
-        super(props)
-     
-        this.children = props.children;
-        this.type= props.type;
-        this.onClick = props.onClick;
-        this.buttonStyle = props.buttonStyle;
-        this.buttonSize = props.buttonSize;
-
+        super(props);
     }
 
-    Button = () => {
+    Button = ({buttonType}: ButtonProps) => {
         // check button style
-        const checkButtonStyle = this.styles.includes(this.buttonStyle)
-        ? this.buttonStyle
+        {buttonType.map(buttonAttr => {
+          const checkButtonStyle = this.styles.includes(buttonAttr.buttonStyle)
+        ? buttonAttr.buttonStyle
         : this.styles[0];
 
         // check button size
-        const checkButtonSize = this.sizes.includes(this.buttonSize)
-        ? this.buttonSize
+        const checkButtonSize = this.sizes.includes(buttonAttr.buttonSize)
+        ? buttonAttr.buttonSize
         : this.sizes[0];
 
         return (
             <Link to='/sign-up' className='btn-mobile'>
               <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-                onClick={this.onClick}
-                type={this.type}
+                onClick={buttonAttr.onClick}
+                type={buttonAttr.type}
               >
-                {this.children}
+                {buttonAttr.children}
               </button>
             </Link>
           );
+
+
+
+
+        })}
 
     
     
--- src/comps/Navbar/Navbar.tsx
+++ src/comps/Navbar/Navbar.tsx
@@ -1,120 +1,130 @@
-import { toHash } from 'ajv/dist/compile/util';
+
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 
 
+interface NavbarProps{
+
+}
+
+
+interface NavbarState{
+  click: boolean,
+  button: boolean,
+};
 
 
 
-export default class Navbar extends Component{
+export default class Navbar extends Component<NavbarProps, NavbarState>{
 
-  private clickAndButton;
+  
 
-  constructor(props){
+  constructor(props: NavbarProps){
     super(props);
-
+    
     this.state = {
       click: false,
-      button:true
+      button: true
     };
+     
+  }
+  
 
-    this.clickAndButton = this.state;
+  private setClick(clickArg: boolean): void{
+    this.setState((NavbarProps, NavbarState) => {
+      click: clickArg;
+    })
+  } 
 
-    
+  protected handleClick = () => {
+      this.setClick(!this.state.click);
   }
 
-    
-    private setClick(clickArg: boolean): void{
-      this.clickAndButton.click = clickArg;
-    } 
+  private setButton(buttonArg: boolean):void{
+    this.setState((NavbarProps, NavbarState) => {
+      button: buttonArg;
+    })
+  }
 
-    protected handleClick = () => {
-        this.setClick(!this.clickAndButton.click;);
-    }
+  protected closeMobileMenu = () =>{
+      this.setClick(!this.state.click);
+  }
 
-    private setButton(buttonArg: boolean):void{
-      this.clickAndButton.button = buttonArg;
-    }
+  protected showButton = () => {
+      if (window.innerWidth <= 960){
+          this.setButton(false);
+      }else{
+          this.setButton(true);
+      }
+  }
 
-    protected closeMobileMenu = () =>{
-        this.setClick(!this.clickAndButton.click);
-    }
-
-    protected showButton = () => {
-        if (window.innerWidth <= 960){
-            this.setButton(false);
-        }else{
-            this.setButton(true);
-        }
-    }
-
-    componentWillMount = () =>{
-        // call showButton 
-        this.showButton();
-        window.addEventListener('resize', this.showButton);
-    }
-    
-    public render(){
-        return(
-            <>
-            
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={this.closeMobileMenu}>
-            TRVL
-            <i className='fab fa-typo3' />
-          </Link>
-          <div className='menu-icon' onClick={this.handleClick}>
-            <i className={this.state.click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={this.state.click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={this.closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/services'
-                className='nav-links'
-                onClick={this.closeMobileMenu}
-              >
-                Services
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={this.closeMobileMenu}
-              >
-                Products
-              </Link>
-            </li>
-
-            <li>
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={this.closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
-          </ul>
-          {<button/> && <Button buttonStyle='btn--outline'>SIGN UP</Button>}  
+  componentWillMount = () =>{
+      // call showButton 
+      this.showButton();
+      window.addEventListener('resize', this.showButton);
+  }
+  
+  public render(){
+      return(
+          <>
           
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to='/' className='navbar-logo' onClick={this.closeMobileMenu}>
+          TRVL
+          <i className='fab fa-typo3' />
+        </Link>
+        <div className='menu-icon' onClick={this.handleClick}>
+          <i className={this.state.click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
-      </nav>
-    </>    
-            
-        );
-    }
+        <ul className={this.state.click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={this.closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/services'
+              className='nav-links'
+              onClick={this.closeMobileMenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/products'
+              className='nav-links'
+              onClick={this.closeMobileMenu}
+            >
+              Products
+            </Link>
+          </li>
 
-    // {<button/> && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-    
-   
+          <li>
+            <Link
+              to='/sign-up'
+              className='nav-links-mobile'
+              onClick={this.closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        {this.state.button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}  
+        
+      </div>
+    </nav>
+  </>    
+          
+      );
+  }
+
+  // {<button/> && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+  
+  
     
 
 }
